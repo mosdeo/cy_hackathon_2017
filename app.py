18b4059cf376a77b60dd8837d70dd3b5d6e74a9e
Lin, Kao-Yuan <mosdeo@gmail.com>
2018-03-23 02:11:09 +0800

    add Line echo bot example

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+#====================== for Line Bot SDK =================
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+#===========================================================
+
 from flask import Flask, request, render_template
 import logging
 import logging.config
@@ -5,6 +19,40 @@ import yaml
 from kh import predict
 
 app = Flask(__name__, static_url_path='')
+
+my_CHANNEL_ACCESS_TOKEN = 'Nejo7izbm3I94qsFsvVeHrJmQ8KGVHQWjk0Z7RdFBDzrUzPCGwmrYNzO7LG/a+ZiNkcDGStjtvcvy8ASyXPEYy8JsrevPPBALpRLkWstbkNb3G702j+eILRA+tm4Ikt/ElI6UPZ8fv4gT7BRdwHq9AdB04t89/1O/w1cDnyilFU=' #open('channel_access_keys/channel_access_token.ini').read()
+print(my_CHANNEL_ACCESS_TOKEN)
+
+my_CHANNEL_SECRET = '123b64f1cbb589326196a5683e98f663' #open("channel_access_keys/channel_access_secret.ini").read()
+print(my_CHANNEL_SECRET)
+
+line_bot_api = LineBotApi(my_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(my_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 try:
     with open('logging.yml') as fd:
